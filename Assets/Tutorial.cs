806a4380a52713cd22f49c03f6355ad38f022eef
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-02-01 19:56:48 -0500

    Saves of you did turtorial
    Saves of you did tutorial by using PlayerPrefs.
    (beta ver. 1.3.0.7)

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -6,15 +6,18 @@ public class Tutorial : MonoBehaviour
 {
     public Animator tutorial;
 
-    bool tutorialIsOver = false;
+    int tutorialIsOver = 0; // 0 = false; 1 = true
 
     // Start is called before the first frame update
     void Start()
     {
-        if (tutorialIsOver == false)
+        tutorialIsOver = PlayerPrefs.GetInt("isOver", 0);
+        if (tutorialIsOver == 0)
         {
             Debug.Log("Tutorial Started");
             tutorial.SetTrigger("Start");
+            PlayerPrefs.SetInt("isOver", 1);
+            tutorialIsOver = PlayerPrefs.GetInt("isOver", 0);
         }
     }
 }
